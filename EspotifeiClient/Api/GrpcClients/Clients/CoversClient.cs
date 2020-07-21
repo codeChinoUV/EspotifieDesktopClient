87e225d40fc57529897ad007b60af71911ed08d6
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-21 14:29:41 -0500

    Se modifica el manejo de excepciones
    Se modifica la forma en la que se manejan las excepciones producidas por el cliente

--- EspotifeiClient/Api/GrpcClients/Clients/CoversClient.cs
+++ EspotifeiClient/Api/GrpcClients/Clients/CoversClient.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
+using Api.Rest.ApiClient;
 using Api.Util;
 using Google.Protobuf;
 using Grpc.Core;
@@ -11,36 +13,101 @@ namespace Api.GrpcClients.Clients
     {
 
         private const int ChunkSize = 16 * 1000;
+        private const int TryNumbres = 2;
 
-        public void UploadUserCover(string path)
+        public async void UploadUserCover(string path)
         {
-            Channel channel = new Channel("192.168.0.9:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel(Configuration.URIGrpcServer, ChannelCredentials.Insecure);
             var client = new Portadas.PortadasClient(channel);
             var call = client.SubirPortadaUsuario();
-            UploadCover(path, 0, call);
-
+            for (int i = 1; i <= TryNumbres; i++)
+            {
+                try
+                {
+                    var response = await UploadCover(path, 0, call);
+                    if (response.Error == Error.TokenInvalido || response.Error == Error.TokenFaltante)
+                    {
+                        ApiServiceLogin.GetServiceLogin().ReLogin();
+                    }
+                    else if (response.Error == Error.Ninguno)
+                    {
+                        break;   
+                    }
+                    else
+                    {
+                        ErrorHandlerUploadCover(response);
+                    }
+                }
+                catch (RpcException ex)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
+            }
         }
 
-        public void UploadAlbumCover(string path, int idAlbum)
+        public async void UploadAlbumCover(string path, int idAlbum)
         {
-            Channel channel = new Channel("192.168.0.9:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel(Configuration.URIGrpcServer, ChannelCredentials.Insecure);
             var client = new Portadas.PortadasClient(channel);
             var call = client.SubirPortadaAlbum();
-            UploadCover(path, idAlbum, call);
+            for (int i = 1; i <= TryNumbres; i++)
+            {
+                try
+                {
+                    var response = await UploadCover(path, idAlbum, call);
+                    if (response.Error == Error.TokenInvalido || response.Error == Error.TokenFaltante)
+                    {
+                        ApiServiceLogin.GetServiceLogin().ReLogin();
+                    }
+                    else if (response.Error == Error.Ninguno)
+                    {
+                        break;   
+                    }
+                    else
+                    {
+                        ErrorHandlerUploadCover(response);
+                    }
+                }
+                catch (RpcException ex)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
+            }
         }
 
-        public void UploadContentCreatorCover(string path, int idContentCreator)
+        public async void UploadContentCreatorCover(string path, int idContentCreator)
         {
-            Channel channel = new Channel("192.168.0.9:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel(Configuration.URIGrpcServer, ChannelCredentials.Insecure);
             var client = new Portadas.PortadasClient(channel);
             var call = client.SubirPortadaCreadorDeContenido();
-            UploadCover(path, idContentCreator, call);
+            for (int i = 1; i <= TryNumbres; i++)
+            {
+                try
+                {
+                    var response = await UploadCover(path, idContentCreator, call);
+                    if (response.Error == Error.TokenInvalido || response.Error == Error.TokenFaltante)
+                    {
+                        ApiServiceLogin.GetServiceLogin().ReLogin();
+                    }
+                    else if (response.Error == Error.Ninguno)
+                    {
+                        break;   
+                    }
+                    else
+                    {
+                        ErrorHandlerUploadCover(response);
+                    }
+                }
+                catch (RpcException ex)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
+            }
         }
 
-        private async void UploadCover(string path, int idCover,
+        private async Task<RespuestaSolicitudSubirArchivo> UploadCover(string path, int idCover,
             AsyncClientStreamingCall<SolicitudSubirPortada, RespuestaSolicitudSubirArchivo> call)
         {
-
             var extension = Path.GetExtension(path).Replace(".", "");
             var formatImage = ConvertExtensionToFormatoImagen(extension);
             if (File.Exists(path))
@@ -49,36 +116,36 @@ namespace Api.GrpcClients.Clients
                 requestUploadCover.InformacionPortada = new InformacionPortada();
                 requestUploadCover.InformacionPortada.IdElementoDePortada = idCover;
                 requestUploadCover.InformacionPortada.FormatoImagen = formatImage;
-                requestUploadCover.TokenAutenticacion =
-                    "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF91c3VhcmlvIjoyLCJleHAiOjE1OTQ2MzExNjV9.j9Pjr6x8NRdKFxFOB84T4jNUvEx2NVdbmvG2HTpcFe8";
+                requestUploadCover.TokenAutenticacion = ApiServiceLogin.GetServiceLogin().GetAccessToken();
                 var coverBytes = FileManager.ByteArrayFromImageFile(path);
                 using (call)
                 {
+                    var totalChunks = coverBytes.Length / ChunkSize;
+                    var finalBytes = coverBytes.Length % ChunkSize;
                     try
                     {
-                        var totalChunks = coverBytes.Length / ChunkSize;
-                        var finalBytes = coverBytes.Length % ChunkSize;
                         for (int i = 0; i < totalChunks; i++)
                         {
                             requestUploadCover.Data =
                                 ByteString.CopyFrom(FileManager.SubArray(coverBytes, i * ChunkSize, ChunkSize));
                             await call.RequestStream.WriteAsync(requestUploadCover);
                         }
-
                         requestUploadCover.Data =
                             ByteString.CopyFrom(FileManager.SubArray(coverBytes, totalChunks, finalBytes));
                         await call.RequestStream.WriteAsync(requestUploadCover);
                         await call.RequestStream.CompleteAsync();
                     }
-                    catch (Exception ex)
+                    catch (RpcException ex)
                     {
-                        var exe = ex.Message;
+                        if (ex.StatusCode != StatusCode.OK)
+                        {
+                            throw new RpcException(ex.Status, ex.Message);
+                        }
                     }
-
-                    var response = await call.ResponseAsync;
-                    var error = response;
+                    return await call.ResponseAsync;
                 }
             }
+            throw new Exception("No se encontro el archivo en la ruta especifica");
         }
 
         private SolicitudObtenerPortada CreateSolicitudObtenerPortada(int idElement, string token)
@@ -90,41 +157,91 @@ namespace Api.GrpcClients.Clients
             return request;
         }
 
-        public async void GetAlbumCover(int idAlbum)
+        public async Task<MemoryStream> GetAlbumCover(int idAlbum)
         {
-            Channel channel = new Channel("192.168.0.10:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel(Configuration.URIGrpcServer, ChannelCredentials.Insecure);
             var client = new Portadas.PortadasClient(channel);
-            var request = CreateSolicitudObtenerPortada(idAlbum,
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF91c3VhcmlvIjoyLCJleHAiOjE1OTQ4NjY4NDF9.v-Xfqaw4WT2WFWhv7kJa3yVrJVxIwj3NXBi6HmRz0uI");
+            var request = CreateSolicitudObtenerPortada(idAlbum,ApiServiceLogin.GetServiceLogin().GetAccessToken());
             var call = client.ObtenerPortadaAlbum(request);
-            GetCover(call);
+            MemoryStream cover = null;
+            for (int i = 1; i <= TryNumbres; i++)
+            {
+                try
+                {
+                    cover = await GetCover(call);
+                    break;
+                }
+                catch (RpcException ex)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
+                catch (Exception)
+                {
+                    ApiServiceLogin.GetServiceLogin().ReLogin();
+                }
+            }
+
+            return cover;
         }
 
-        public async void GetContentCreatorCover(int idContentCreator)
+        public async Task<MemoryStream> GetContentCreatorCover(int idContentCreator)
         {
-            Channel channel = new Channel("192.168.0.10:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel(Configuration.URIGrpcServer, ChannelCredentials.Insecure);
             var client = new Portadas.PortadasClient(channel);
-            var request = CreateSolicitudObtenerPortada(idContentCreator,
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF91c3VhcmlvIjoyLCJleHAiOjE1OTQ4NjY4NDF9.v-Xfqaw4WT2WFWhv7kJa3yVrJVxIwj3NXBi6HmRz0uI");
+            var request = CreateSolicitudObtenerPortada(idContentCreator,ApiServiceLogin.GetServiceLogin().GetAccessToken());
             var call = client.ObtenerPortadaCreadorDeContenido(request);
-            GetCover(call);
+            MemoryStream cover = null;
+            for (int i = 1; i <= TryNumbres; i++)
+            {
+                try
+                {
+                    cover = await GetCover(call);
+                    break;
+                }
+                catch (RpcException ex)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
+                catch (Exception)
+                {
+                    ApiServiceLogin.GetServiceLogin().ReLogin();
+                }
+            }
+
+            return cover;
         }
 
-        public async void GetUserCover(int idUser)
+        public async Task<MemoryStream> GetUserCover(int idUser)
         {
-            Channel channel = new Channel("192.168.0.10:5001", ChannelCredentials.Insecure);
+            Channel channel = new Channel(Configuration.URIGrpcServer, ChannelCredentials.Insecure);
             var client = new Portadas.PortadasClient(channel);
             var request = CreateSolicitudObtenerPortada(idUser,
-                "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZF91c3VhcmlvIjoyLCJleHAiOjE1OTQ4NjY4NDF9.v-Xfqaw4WT2WFWhv7kJa3yVrJVxIwj3NXBi6HmRz0uIY");
+                ApiServiceLogin.GetServiceLogin().GetAccessToken());
             var call = client.ObtenerPortadaUsuario(request);
-            GetCover(call);
+            MemoryStream cover = null;
+            for (int i = 1; i <= TryNumbres; i++)
+            {
+                try
+                {
+                    cover = await GetCover(call);
+                    break;
+                }
+                catch (RpcException ex)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
+                catch (Exception)
+                {
+                    ApiServiceLogin.GetServiceLogin().ReLogin();
+                }
+            }
+
+            return cover;
         }
 
-        private async void GetCover(AsyncServerStreamingCall<RespuestaObtenerPortada> call)
+        private async Task<MemoryStream> GetCover(AsyncServerStreamingCall<RespuestaObtenerPortada> call)
         {
             MemoryStream memoryStream = new MemoryStream();
-            int position = 0;
-            FormatoImagen formatImage = FormatoImagen.Png;
             Error error = Error.Ninguno;
             try
             {
@@ -134,22 +251,29 @@ namespace Api.GrpcClients.Clients
                     {
                         RespuestaObtenerPortada response = call.ResponseStream.Current;
                         memoryStream.Write(response.Data.ToByteArray(), 0, response.Data.Length);
-                        position += response.Data.Length;
-                        formatImage = response.FormatoPortada;
                         error = response.Error;
                     }
                 }
             }
-            catch (Exception ex)
+            catch (RpcException ex)
             {
-                var exe = ex;
+                if (ex.StatusCode != StatusCode.OK)
+                {
+                    throw new RpcException(ex.Status, ex.Message);
+                }
             }
-            var error1 = error;
-            var totalSize = memoryStream.ToArray().Length;
-            Console.WriteLine(totalSize);
+
+            if (error != Error.Ninguno && (error == Error.TokenFaltante || error == Error.TokenInvalido))
+            {
+                ErrorHandlerGetCover(error);
+            }else if (error != Error.Ninguno)
+            {
+                memoryStream = null;
+            }
+
+            return memoryStream;
         }
-        
-        
+
         private FormatoImagen ConvertExtensionToFormatoImagen(string extension)
         {
             var formatImage = FormatoImagen.Png;
@@ -163,6 +287,40 @@ namespace Api.GrpcClients.Clients
 
             return formatImage;
         }
+
+        /// <summary>
+        /// Se encarga de manejar las posibles excepciones que puedan ocurrir al subir o solicitar una portada
+        /// </summary>
+        /// <param name="respuesta"></param>
+        /// <exception cref="Exception"></exception>
+        private void ErrorHandlerUploadCover(RespuestaSolicitudSubirArchivo respuesta)
+        {
+            switch (respuesta.Error)
+            {
+                case Error.Desconocido:
+                    throw new Exception("Ocurrio un error en el servidor y no se pudo guardar la imagen");
+                case Error.OperacionNoPermitida:
+                    throw new Exception("No tiene permiso a realizar la operacion solicitada");
+                case Error.AlbumInexistente:
+                    throw new Exception("No existe el album al que desea agregar la portada");
+            }
+        }
+
+        /// <summary>
+        /// Se encarga de manejar los errores ocurridos al recuperar una cancion
+        /// </summary>
+        /// <param name="error">El error ocurrido</param>
+        /// <exception cref="Exception">La excepcion que indica el tipo de error ocurrido</exception>
+        private void ErrorHandlerGetCover(Error error)
+        {
+            switch (error)
+            {
+                case Error.TokenFaltante:
+                case Error.TokenInvalido:
+                    throw new Exception();
+            }
+        }
+        
     }
     
     

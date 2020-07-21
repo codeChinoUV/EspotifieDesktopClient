adb298b84c2ea15cbcf7fc0c07941de2b5a666a3
Rafael Andrade Méndez <rafithahardy@gmail.com>
2020-07-21 11:25:31 -0500

    Creación de clase CreadorContenido
    Se creó la clase CreadorContenido con sus respectivos atributos.

---
+++ EspotifeiClient/Model/CreadorContenido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class CreadorContenido
+    {
+        public string nombre { get; set; }
+
+        public string biografia { get; set; }
+
+        public List<String> generos { get; set; }
+
+        public bool es_grupo { get; set; }
+
+    }
+}

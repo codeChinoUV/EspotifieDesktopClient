92138d793ca903c025730e34970e517c1e00078a
Rafael Andrade Méndez <rafithahardy@gmail.com>
2020-07-21 23:36:39 -0500

    Creación de clase Genero y GeneroClient
    Se crearon las clases Genero y GeneroClient para trabajar en el CU Registrar Creador de Contenido

---
+++ EspotifeiClient/Model/Genero.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    public class Genero
+    {
+        public int id { get; set; }
+        public string genero { get; set; }
+    }
+}

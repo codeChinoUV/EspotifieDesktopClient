9b110ebff2e96a1bf012f6ae8d76db45b3c6e502
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-21 23:21:46 -0500

    Se agrega la barra de navegacion y se elimina la pagina barra de navegacion

--- EspotifeiClient/EspotifeiClient/MenuInicio.xaml.cs
+++ EspotifeiClient/EspotifeiClient/MenuInicio.xaml.cs
@@ -1,18 +1,48 @@
-﻿using System.Windows;
-using System.Windows.Controls;
+﻿
+using System.Windows;
 
 namespace EspotifeiClient
 {
     /// <summary>
     /// Lógica de interacción para MenuInicio.xaml
     /// </summary>
-    public partial class MenuInicio : Page
+    public partial class MenuInicio
     {
         public MenuInicio()
         {
             InitializeComponent();
         }
 
+        private void AbrirMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            abrirMenuButton.Visibility = Visibility.Collapsed;
+            cerrarMenuButton.Visibility = Visibility.Visible;
+        }
 
+        private void CerrarMenuButton_Click(object sender, RoutedEventArgs e)
+        {
+            abrirMenuButton.Visibility = Visibility.Visible;
+            cerrarMenuButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void CerrarButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BuscarListview_Selected(object sender, RoutedEventArgs e)
+        {
+            new IniciarSesion();
+        }
+
+        private void ListaReproduccionListview_Selected(object sender, RoutedEventArgs e)
+        {
+            new RegistrarPlaylist().Show();
+        }
+
+        private void AbrirMenuButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }

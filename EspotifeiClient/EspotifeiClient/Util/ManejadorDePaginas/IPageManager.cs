bf2c5141ddde2e165bf454c61964b8463a3c65a6
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-21 23:19:32 -0500

    Se crea el page manager

---
+++ EspotifeiClient/EspotifeiClient/Util/ManejadorDePaginas/IPageManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows.Controls;
+
+namespace EspotifeiClient.Util.ManejadorDePaginas
+{
+    /// <summary>
+    ///     Interfaz que proporciona la habilidad de cambiar de pantalla en una ventana.
+    /// </summary>
+    public interface IPageManager
+    {
+        /// <summary>
+        ///     Realiza un cambio de página en la ventana que implemente esta interfaz. Acomoda el tamaño de la ventana
+        ///     al tamaño de la página y regresa una referencia a la instancia recién creada.
+        /// </summary>
+        /// <typeparam name="T">Tipo de página, que implemente IPageListener</typeparam>
+        /// <returns>La instancia de la página</returns>
+        T ChangePage<T>() where T : UIPage, IPageListener, new();
+        
+    }
+}
---
+++ EspotifeiClient/EspotifeiClient/Util/ManejadorDePaginas/UIWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace EspotifeiClient.Util.ManejadorDePaginas
+{
+    public class UIWindow : Window, IPageManager
+    {
+
+        public T ChangePage<T>() where T : UIPage, IPageListener, new()
+        {
+            var page = new T();
+            page.SetPageManager(this);
+            Content = page;
+            return page;
+        }
+        
+    }
+}

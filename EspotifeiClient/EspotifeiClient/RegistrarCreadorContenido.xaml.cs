ffa0dccd391fcedd2bd5259ecf147f72d6cac5e9
Rafael Andrade Méndez <rafithahardy@gmail.com>
2020-07-22 01:35:32 -0500

    Validaciones para la creación de Creador de Contenido
    Se crearon las validaciones pertinentes para realizar la creación de un Creador de Contenido

--- EspotifeiClient/EspotifeiClient/RegistrarCreadorContenido.xaml.cs
+++ EspotifeiClient/EspotifeiClient/RegistrarCreadorContenido.xaml.cs
@@ -3,6 +3,10 @@ using System;
 using System.Windows;
 using System.Net.Http;
 using Api.Rest;
+using EspotifeiClient.Util;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using Api.GrpcClients.Clients;
 
 namespace EspotifeiClient
 {
@@ -11,6 +15,7 @@ namespace EspotifeiClient
     /// </summary>
     public partial class RegistrarCreadorContenido
     {
+        private string _rutaImagen = "";
         public RegistrarCreadorContenido()
         {
             InitializeComponent();
@@ -21,22 +26,13 @@ namespace EspotifeiClient
         /// </summary>
         /// <returns>Variable de tipo CreadorContenido</returns>
         private CreadorContenido CrearCreadorContenido()
-        {
-            bool grupo = false;
-
-            /*
-             NO SUBIR CODIGO CON ERRORES, SI ALGO NO FUNCIONA SE COMENTA Y SE PONE UN TODO 
-             if ((bool)grupoCheckbox.IsChecked)
-            {
-                grupo = true;
-            }
-
-            var creadorContenido = new CreadorContenido
+        { 
+            /*var creadorContenido = new CreadorContenido
             {
                 nombre = nombreCreadorTextbox.Text,
                 biografia = biografiaTextbox.Text,
                 generos = generosCreador,
-                es_grupo = grupo,
+                es_grupo = ValidarCheckBoxGrupo(),
             };*/
             return null;
         }
@@ -48,21 +44,29 @@ namespace EspotifeiClient
         /// <param name="e"></param>
         private async void OnClickRegistrarCreadorButton(object sender, RoutedEventArgs e)
         {
-            cancelarButton.IsEnabled = false;
-            registrarCreadorButton.IsEnabled = false;
-            var creador = CrearCreadorContenido();
-            try
+            if (ValidarTextBoxNombre() && ValidarTextBoxBiografia())
             {
-                await CreadorContenidoClient.RegisterCreadorContenido(creador);
-            } catch (HttpRequestException)
-            {
-                new MensajeEmergente().MostrarMensajeError("No se puede conectar al servidor");
-            } catch (Exception exception)
-            {
-                new MensajeEmergente().MostrarMensajeAdvertencia(exception.Message);
+                cancelarButton.IsEnabled = false;
+                registrarCreadorButton.IsEnabled = false;
+                var creador = CrearCreadorContenido();
+                try
+                {
+                    await CreadorContenidoClient.RegisterCreadorContenido(creador);
+                    if (_rutaImagen != "")
+                    {
+                        var clientePortadas = new CoversClient();
+                        clientePortadas.UploadUserCover(_rutaImagen);
+                    }
+                } catch (HttpRequestException)
+                {
+                    new MensajeEmergente().MostrarMensajeError("No se puede conectar al servidor");
+                } catch (Exception exception)
+                {
+                    new MensajeEmergente().MostrarMensajeAdvertencia(exception.Message);
+                }
+                cancelarButton.IsEnabled = true;
+                registrarCreadorButton.IsEnabled = true;
             }
-            cancelarButton.IsEnabled = true;
-            registrarCreadorButton.IsEnabled = true;
         }
 
         /// <summary>
@@ -78,8 +82,90 @@ namespace EspotifeiClient
             {
                 new MensajeEmergente().MostrarMensajeError("No se puede conectar al servidor");
             }
+        }
 
-         
+        /// <summary>
+        /// Método que valida el tamaño del campo nombreCreadorTextbox
+        /// </summary>
+        /// <returns>Verdadero si el TextBox tiene una longitud valida</returns>
+        private bool ValidarTextBoxNombre()
+        {
+            var tamañoMinimo = 5;
+            var tamañoMaximo = 70;
+            var nombre = nombreCreadorTextbox.Text;
+            var esValido = ValidacionDeCadenas.ValidarTamañoDeCadena(nombre, tamañoMinimo, tamañoMaximo);
+            if (!esValido)
+            {
+                new MensajeEmergente().MostrarMensajeAdvertencia(
+                    $"El campo de nombre debe de tener mas de {tamañoMinimo} caracteres y menos de {tamañoMaximo}");
+            }
+            return esValido;
+        }
+
+        /// <summary>
+        /// Método que valida el tamaño del campo biografiaTextbox
+        /// </summary>
+        /// <returns>Verdadero si el TextBox tiene una longitud valida</returns>
+        private bool ValidarTextBoxBiografia()
+        {
+            var tamañoMinimo = 5;
+            var tamañoMaximo = 500;
+            var biografia = biografiaTextbox.Text;
+            var esValido = ValidacionDeCadenas.ValidarTamañoDeCadena(biografia, tamañoMinimo, tamañoMaximo);
+            if (!esValido)
+            {
+                new MensajeEmergente().MostrarMensajeAdvertencia(
+                    $"El campo de biografía debe de tener mas de {tamañoMinimo} caracteres y menos de {tamañoMaximo}");
+            }
+            return esValido;
+        }
+
+        /// <summary>
+        /// Método que valida si el CheckBox del grupo está activado o no
+        /// </summary>
+        /// <returns>Verdadero si el CheckBox está activado</returns>
+        private bool ValidarCheckBoxGrupo()
+        {
+            var grupo = false;
+
+            if ((bool) grupoCheckbox.IsChecked)
+            {
+                grupo = true;
+            } else
+            {
+                return grupo;
+            }
+            return grupo;
+        }
+
+        /// <summary>
+        /// Método que contiene el evento para abrir el explorador de archivos y cargar una imagen
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnClickAgregarImagenImage(object sender, RoutedEventArgs e)
+        {
+            BitmapDecoder bitmap;
+            var abrirImagen = new OpenFileDialog();
+            abrirImagen.Filter = "Archivos png (*.png)|*.png|Archivos jpg (*.jpg)|*.jpg";
+            var archivoSeleccionado = abrirImagen.ShowDialog();
+            if (archivoSeleccionado != null)
+            {
+                try
+                {
+                    if ((bool) archivoSeleccionado)
+                        using (var stream = abrirImagen.OpenFile())
+                        {
+                            bitmap = BitmapDecoder.Create(stream, BitmapCreateOptions.PreservePixelFormat,
+                                BitmapCacheOption.OnLoad);
+                            portadaCreadorImage.Source = bitmap.Frames[0];
+                            _rutaImagen = abrirImagen.FileName;
+                        }
+                } catch (Exception)
+                {
+                    new MensajeEmergente().MostrarMensajeError("Tipo de imagen invalida");
+                }
+            }
         }
     }
 }

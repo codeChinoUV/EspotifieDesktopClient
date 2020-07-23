b56bc8df09abcf9a9da7e9ca864b94672e988679
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-23 00:12:33 -0500

    Se ha cambiado la creacion de imagen a traves de un memoryStream a otra clase

---
+++ EspotifeiClient/EspotifeiClient/Util/ImagenUtil.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace EspotifeiClient.Util
+{
+    public class ImagenUtil
+    {
+        /// <summary>
+        /// Crea un BitmapImage a partir de un MemoryStream
+        /// </summary>
+        /// <param name="imagen">El MemroyStream que contiene la imagen</param>
+        /// <returns>Un BitmapImage</returns>
+        public static BitmapImage CrearBitmapDeMemory(MemoryStream imagen)
+        {
+            var bitmapImage = new BitmapImage();
+            using (imagen)
+            {
+                imagen.Position = 0;
+                bitmapImage.BeginInit();
+                bitmapImage.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.UriSource = null;
+                bitmapImage.StreamSource = imagen;
+                bitmapImage.EndInit();
+            }
+            bitmapImage.Freeze();
+            return bitmapImage;
+        }
+    }
+}

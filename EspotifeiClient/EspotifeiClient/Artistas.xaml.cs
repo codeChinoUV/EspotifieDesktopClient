755374892802b9d0bb804c087faef4f6161b0559
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-24 02:46:57 -0500

    Se modifica la forma de recuperar las portadas

--- EspotifeiClient/EspotifeiClient/Artistas.xaml.cs
+++ EspotifeiClient/EspotifeiClient/Artistas.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows.Media.Imaging;
 using Api.GrpcClients.Clients;
 using Api.Rest;
 using EspotifeiClient.Util;
-using Grpc.Core;
+using ManejadorDeArchivos;
 using Model;
 
 namespace EspotifeiClient
@@ -18,44 +18,28 @@ namespace EspotifeiClient
     {
 
         private List<CreadorContenido> _creadoresContenidos;
-        
+
         public Artistas()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Recupera la busqueda de los creadores de contenido que coincidan con el buscarTextBox
+        /// </summary>
+        /// <param name="sender">El objeto que invoco el evento</param>
+        /// <param name="keyEventArgs">El evento invocado</param>
         public async void BuscarCreadoresDeContenido(object sender, KeyEventArgs keyEventArgs)
         {
             var cadenaBusqueda = buscarTextBox.Text;
-            var clientePortadas = new CoversClient();
             if (cadenaBusqueda != "")
             {
                 try
                 {
                     _creadoresContenidos = await CreadorContenidoClient.SearchCreadorContenido(cadenaBusqueda);
-                    foreach (var creador in _creadoresContenidos)
-                    {
-                        try
-                        {
-                            var bitmap = await clientePortadas.GetContentCreatorCover(creador.id);
-                            if (bitmap != null)
-                            {
-                                creador.PortadaImagen = ImagenUtil.CrearBitmapDeMemory(bitmap);
-                            }
-                            else
-                            {
-                                creador.PortadaImagen = (BitmapImage) FindResource("ArtistaDesconocidoImagen");
-                            }
-                        }
-                        catch (Exception)
-                        {
-                            creador.PortadaImagen = (BitmapImage) FindResource("ArtistaDesconocidoImagen");
-                        }
-                    }
-
                     CreadoresDeContenidoListView.ItemsSource = _creadoresContenidos;
-                }
-                catch (HttpRequestException)
+                    ColocarImagenesArtistas();
+                }catch (HttpRequestException)
                 {
                     //Colocar en modo sin conexion
                 }
@@ -69,13 +53,45 @@ namespace EspotifeiClient
                 _creadoresContenidos = new List<CreadorContenido>();
             }
         }
+        
+        /// <summary>
+        /// Recupera las imagenes de los artistas
+        /// </summary>
+        private async void ColocarImagenesArtistas()
+        {
+            CreadoresDeContenidoListView.IsEnabled = false;
+            var clientePortadas = new CoversClient();
+            foreach (var creador in _creadoresContenidos)
+            {
+                try
+                {
+                    var bitmap = await clientePortadas.GetContentCreatorCover(creador.id, Calidad.Baja);
+                    if (bitmap != null)
+                    {
+                        creador.PortadaImagen = ImagenUtil.CrearBitmapDeMemory(bitmap);
+                    }
+                    else
+                    {
+                        creador.PortadaImagen = (BitmapImage) FindResource("ArtistaDesconocidoImagen");
+                    }
+                }
+                catch (Exception)
+                {
+                    creador.PortadaImagen = (BitmapImage) FindResource("ArtistaDesconocidoImagen");
+                }
+            }
 
+            CreadoresDeContenidoListView.ItemsSource = null;
+            CreadoresDeContenidoListView.ItemsSource = _creadoresContenidos;
+            CreadoresDeContenidoListView.IsEnabled = true;
+        }
+        
         private void OnSelectedItem(object sender, MouseButtonEventArgs e)
         {
             var creadorDeContenido = (CreadorContenido) CreadoresDeContenidoListView.SelectedItem;
             if (creadorDeContenido != null)
             {
-                NavigationService.Navigate(new ArtistaElementos(creadorDeContenido));
+                NavigationService?.Navigate(new ArtistaElementos(creadorDeContenido));
             }
         }
     }

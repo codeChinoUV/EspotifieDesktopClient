5a1a36e29dca733b4430649104fbfd215dd13fd5
Rafael Andrade Méndez <rafithahardy@gmail.com>
2020-07-24 21:22:37 -0500

    Creación de clase ListaReproduccion
    Se creó la clase ListaReproduccion en el Modelo con sus respectivos atributos

---
+++ EspotifeiClient/Model/ListaReproduccion.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media.Imaging;
+
+namespace Model
+{
+    public class ListaReproduccion
+    {
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+
+        public string descripcion { get; set; }
+
+        public int duracion_total { get; set; }
+
+        public BitmapImage PortadaImagen { get; set; }
+    }
+}

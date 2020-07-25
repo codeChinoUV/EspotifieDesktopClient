b006b6a9c353eda17481670539858b35e3c97f1f
Rafael Andrade Méndez <rafithahardy@gmail.com>
2020-07-24 21:24:19 -0500

    Creación de clase ListaReproduccionClient
    Se creó la clase ListaReproduccionClient en su primera versión para buscar playlist de acuerdo a una cadena.

---
+++ EspotifeiClient/Api/Rest/ListaReproduccionClient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Model;
+
+namespace Api.Rest
+{
+    public class ListaReproduccionClient
+    {
+        /// <summary>
+        /// Tarea que se encarga de recuperar las listas de producción por búsqueda
+        /// </summary>
+        /// <param name="search">La cadena a buscar</param>
+        /// <returns>Una lista de Listas de reproducción</returns>
+        /// <exception cref="Exception">Alguna excepcion que ocurrió durante la solicitud</exception>
+        public static async Task<List<ListaReproduccion>> SearchListaReproduccion(string search)
+        {
+            var path = $"/v1/listas-de-reproduccion/buscar/{search}";
+            using (HttpResponseMessage response = await ApiClient.GetApiClient().GetAsync(path))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var listasReproduccion = await response.Content.ReadAsAsync<List<ListaReproduccion>>();
+                    return listasReproduccion;
+                } else
+                {
+                    ErrorGeneral error;
+                    error = await response.Content.ReadAsAsync<ErrorGeneral>();
+                    throw new Exception(error.mensaje);
+                }
+            }
+        }
+    }
+}

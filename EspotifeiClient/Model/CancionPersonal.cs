bd2a60d1cde27a613756854d22e6823f56689175
Jose Miguel Quiroz <pumas.chino99@gmail.com>
2020-07-28 19:33:01 -0500

    Se actualiza el modelo de CancionPersonal

--- EspotifeiClient/Model/CancionPersonal.cs
+++ EspotifeiClient/Model/CancionPersonal.cs
@@ -1,7 +1,26 @@
+using System;
+
 namespace Model
 {
     public class CancionPersonal
     {
+        public int id { get; set; }
+
+        public string nombre { get; set; }
         
+        public float duracion { get; set; }
+        
+        public string artistas { get; set; }
+        
+        public string album { get; set; }
+
+        public string duracion_string
+        {
+            get
+            {
+                var time = TimeSpan.FromSeconds(duracion);
+                return time.ToString("mm':'ss");
+            }
+        }
     }
 }

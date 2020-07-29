fedae7a2d0c91eacb980cd75ea1d2cf2ae578806
Rafael Andrade Méndez <rafithahardy@gmail.com>
2020-07-29 02:32:03 -0500

    Actualización de pantalla Registrar Playlist
    Se actualizaron los campos de la pantalla mencionada, sustituyendo Window por Page

--- EspotifeiClient/EspotifeiClient/RegistrarPlaylist.xaml.cs
+++ EspotifeiClient/EspotifeiClient/RegistrarPlaylist.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace EspotifeiClient
 {
     /// <summary>
     ///     Lógica de interacción para RegistrarPlaylist.xaml
     /// </summary>
-    public partial class RegistrarPlaylist : Window
+    public partial class RegistrarPlaylist : Page
     {
         public RegistrarPlaylist()
         {
